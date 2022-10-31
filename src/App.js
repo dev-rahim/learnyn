c9aaea8ce1614efc28fd0c9c0c9868c594c207b0
dev-rahim <rahimuddin5915@gmail.com>
2022-10-31 10:45:24 +0600

    added react routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Components/Home/Home';
+import NotFound from './Components/Home/NotFound/NotFound';
+import About from './Components/About/About';
+import Courses from './Components/Courses/Courses';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Contacts from './Components/Contacts/Contacts';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route exact path='home' element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='courses' element={<Courses />} />
+          <Route path='contacts' element={<Contacts />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Courses/Courses.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Courses.css'
+const Courses = () => {
+    return (
+        <div>
+            <h3>Courses</h3>
+        </div>
+    );
+};
+
+export default Courses;
